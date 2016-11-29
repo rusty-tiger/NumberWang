b4703cb5d9e53b64973c154ad2523696c9a1e182
rusty-tiger <rusty_tiger@hotmail.co.uk>
2016-11-29 01:43:41 +0000

    Added Basic Game Shit
    Basic stuff, doent look good

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+    
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			if (Application.loadedLevelName == "Game") {
+				LoadLevel("Start");
+			} else {
+				QuitRequest();
+			}
+		}
+	}
+	
+	public void LoadLevel(string name) {
+		Debug.Log ("Level REquest: " + name);
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest() {
+		Debug.Log ("Plz Quit");
+		Application.Quit();
+	}
+}
---
+++ Assets/NumberWanger.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberWanger : MonoBehaviour {
+
+	public Text leftText;
+	public Text rightText;
+	public Text playerText;
+	public Text computerText;
+	int playerScore;
+	int computerScore;
+	
+	System.Random getrandom = new System.Random();
+	
+	int rangeLow = -100;
+	int rangeHigh = 100;
+	
+	
+	// Use this for initialization
+	void Start () {
+		playerScore = 0;
+		computerScore = 0;
+		
+		leftText.text = "" + 0;
+	
+		RefreshOptions();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void LeftButtonClicked() { 
+		isNumberWang();
+		RefreshOptions();
+	}
+	public void RightButtonClicked() { 
+		isNumberWang();
+		RefreshOptions();
+	}
+	
+	void isNumberWang() {
+		if (getrandom.Next(0, 100) > 70)
+		{
+			Debug.Log("ThatsNumberWang");
+			++playerScore;
+		}
+		else {
+			++computerScore;
+		}	
+		SetScoreText();
+	}
+	
+	void RefreshOptions() {
+		if (getrandom.Next(0, 100) > 20) {
+			leftText.text = "" + getrandom.Next(rangeLow, rangeHigh);
+		}
+		if (getrandom.Next(0, 100) > 20) {
+		rightText.text = "" + getrandom.Next(rangeLow, rangeHigh);
+		}
+		while ( leftText.text == rightText.text ) 
+			rightText.text = "" + getrandom.Next(rangeLow, rangeHigh);
+	}
+	
+	void SetScoreText() {
+		playerText.text = "You " + playerScore;
+		computerText.text = computerScore + " Computer";
+		
+	}
+}
